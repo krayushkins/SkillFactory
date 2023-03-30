cead3c082add99ce4b2e3b1087b451a1d8b18cb6
krayushkins <126200534+krayushkins@users.noreply.github.com>
2023-03-30 14:00:33 +0300

    Add files via upload

---
+++ 17.7.3.py
@@ -0,0 +1,18 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = []
+money = input('Вводите сумму, которую планируете положить под проценты: ')
+
+s = '_'
+print(s*100)
+
+deposit.append(int(per_cent['ТКБ'] * int(money) / 100))
+deposit.append(int(per_cent['СКБ'] * int(money) / 100))
+deposit.append(int(per_cent['ВТБ'] * int(money) / 100))
+deposit.append(int(per_cent['СБЕР'] * int(money) / 100))
+print('Накопленные средства за год вклада в каждом из банков: ', deposit)
+
+print(s*100)
+
+print('Ваш максимальный заработок может составить: {}'.format(max(deposit)))
+
+print(s*100)
