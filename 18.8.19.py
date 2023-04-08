e7cf81a1b63062fae5dfc0732cb5001627323164
krayushkins <126200534+krayushkins@users.noreply.github.com>
2023-04-08 11:48:45 +0300

    Add files via upload

---
+++ 18.8.19.py
@@ -0,0 +1,16 @@
+tickets = int(input('Количество билетов которое вы хотите купить?\n'))
+tickets_list = [i + 1 for i in range(tickets)]
+result = []
+for ticket in tickets_list:
+    age = int(input('Сколько лет посетителю с билетом №{}\n'.format(ticket)))
+    if age < 18:
+        result.append(0)
+    elif age < 25:
+        result.append(990)
+    else:
+        result.append(1390)
+    if len(result) > 3:
+        sale = int(sum(result) * 10 / 100)
+        print('Сумма к оплате: {} руб.'.format(sum(result) - sale))
+    else:
+        print('Сумма к оплате: {} руб.'.format(sum(result)))
